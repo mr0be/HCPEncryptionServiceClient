3c0e7efaf0ddd7b7b434ae2926ac23f88accb4ab
mr0be <bdt.xus72@gmail.com>
2024-12-27 10:45:13 +0100

    add requirements and refac dec_client

--- http_dec_client.py
+++ http_dec_client.py
@@ -1,15 +1,19 @@
-import requests, sys, os, json
+import requests, sys, os
 import app_dec_client
 from dotenv import load_dotenv
 
 load_dotenv()
 
+def send(data: str) -> requests.Response:
+    response = requests.post( os.getenv('HTTP_SERVER'), 
+                                 auth=(os.getenv('HTTP_SERVER_USERNAME'), os.getenv('HTTP_SERVER_PASSWORD')), 
+                                 headers={"Content-Type": "application/json"}, data=data)
+    return response
+    
 if __name__ == "__main__":
     if len(sys.argv) == 2 and os.path.exists(sys.argv[1]):
         data = app_dec_client.decodeFile(sys.argv[1])
-        response = requests.post( os.getenv('HTTP_SERVER'), 
-                                 auth=(os.getenv('HTTP_SERVER_USERNAME'), os.getenv('HTTP_SERVER_PASSWORD')), 
-                                 headers={"Content-Type": "application/json"}, data=data)
+        response = send(data)
         if response.status_code == 200:
             print("ok:", response.json())
         else:
